862faaa181cb0b4578698aec5d68037b25c6e022
Felipe Amador <felipedavidamador@gmail.com>
2021-03-11 13:38:44 -0300

    add static files support

--- src/urls.py
+++ src/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('apps.posts.urls', namespace='posts'), name='posts')
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
